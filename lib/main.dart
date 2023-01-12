12228dfc7ff73797b794393eaaac31fb406f1ad2
jiho <ghrms80@gmail.com>
2023-01-12 10:29:20 +0900

    Setting up the Home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:ch03_basic_widgets/home.dart';
 import 'package:flutter/material.dart';
-// 1
 import 'package:ch03_basic_widgets/fooderlich_theme.dart';
 
 void main() {
@@ -10,30 +10,12 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({super.key});
   @override
   Widget build(BuildContext context) {
-    // 2
     final theme = FooderlichTheme.dark();
-    // TODO: Apply Home widget
-    // 3
+
     return MaterialApp(
       theme: theme,
       title: 'Fooderlich',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Fooderlich',
-            // 4
-            style: theme.textTheme.headline6,
-          ),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            'Let\'s get cooking',
-            // 5
-            style: theme.textTheme.headline1,
-          ),
-        ),
-      ),
+      home: const Home(),
     );
   }
 }
